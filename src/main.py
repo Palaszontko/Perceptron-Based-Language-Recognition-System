1b8e4e2f21bfe39f5d3d8da76221c6d4113696b1
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-04-03 15:39:46 +0200

    feat: implement main function to initialize perceptrons and measure execution time

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
-print("Hello, World!")
+from dataHandler import DataHandler
+from perceptron import Perceptron
+from layer import Layer
+import time
+import os
+
+ALPHA = 0.1
+THRESHOLD = 0.5
+
+def main():
+    time.start = time.time()
+
+    data = DataHandler.getLanguagesMapToListOfTexts("data")
+
+    languages = data.keys()
+
+    perceptrons = {}
+
+    for language in languages:
+        perceptron = Perceptron(THRESHOLD, ALPHA)
+        perceptrons[language] = perceptron
+
+    layer = Layer(perceptrons)
+
+    print(f'Time taken: {round(time.time() - time.start, 3)}s')
+if __name__ == "__main__":
+    main()
